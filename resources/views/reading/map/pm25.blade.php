e281a916c25f78f2842d3b35740b4de2fe3bdfff
Ochir Sanjaadorj <sor.net@gmail.com>
2017-03-02 15:04:46 +0800

    color code basic

--- resources/views/reading/map/pm25.blade.php
+++ resources/views/reading/map/pm25.blade.php
@@ -3,7 +3,10 @@ if ($sensor->lastreading->pm25 < 50){
   $class='btn-success';
 } elseif ($sensor->lastreading->pm25 < 100){
   $class='btn-primary';
+} elseif ($sensor->lastreading->pm25 < 200){
+  $class='btn-warning';
+} elseif ($sensor->lastreading->pm25 > 200){
+  $class='btn-danger';
 }
-
 @endphp
 <a href="#" class="btn {{$class}} ">{{$sensor->lastreading->pm25}}</a>
